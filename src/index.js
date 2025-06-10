0169a796b941f96ef2b5da09fea04e9c3389a0fd
Rab008 <11872509+rab008@users.noreply.github.com>
2025-06-11 07:09:21 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,36 +1,22 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import { HashRouter } from 'react-router-dom';
-// import './styles/index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter basename="/rizedigitalsite">
-    <App />
-  </BrowserRouter>
-);
-
-
 /**
  * Main entry point for the Rize Digital website
- * Renders the App component wrapped in HashRouter for GitHub Pages compatibility
+ * Renders the App component wrapped in BrowserRouter for routing
  */
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <HashRouter>
-//       <App />
-//     </HashRouter>
-//   </React.StrictMode>
-// );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
