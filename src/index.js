2f1094cf8b3c71572f066166669088f38272017c
Rab008 <11872509+rab008@users.noreply.github.com>
2025-06-11 06:59:08 +0800

    update index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { HashRouter } from 'react-router-dom';
+// import './styles/index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { HashRouter } from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter basename="/rizedigitalsite">
+    <App />
+  </BrowserRouter>
+);
+
 
 /**
  * Main entry point for the Rize Digital website
@@ -18,10 +31,6 @@ import reportWebVitals from './reportWebVitals';
 //     </HashRouter>
 //   </React.StrictMode>
 // );
-
-<BrowserRouter basename="/rizedigitalsite">
-  <App />
-</BrowserRouter>
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
