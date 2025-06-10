fe74df423ab3832a4477cff0f5dd0a0cdb8a7ae9
Rab008 <11872509+rab008@users.noreply.github.com>
2025-06-11 06:53:41 +0800

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+// import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,14 +10,18 @@ import reportWebVitals from './reportWebVitals';
  * Main entry point for the Rize Digital website
  * Renders the App component wrapped in HashRouter for GitHub Pages compatibility
  */
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <HashRouter>
+//       <App />
+//     </HashRouter>
+//   </React.StrictMode>
+// );
+
+<BrowserRouter basename="/rizedigitalsite">
+  <App />
+</BrowserRouter>
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
