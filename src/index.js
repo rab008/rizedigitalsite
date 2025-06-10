63034c6199425e9ea79dbd338b95851ac7ec7c50
Rab008 <11872509+rab008@users.noreply.github.com>
2025-06-11 07:08:29 +0800

    a

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { HashRouter } from 'react-router-dom';
+// import './styles/index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
@@ -6,11 +13,26 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter basename="/rizedigitalsite">
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter basename="/rizedigitalsite">
+    <App />
+  </BrowserRouter>
 );
 
+
+/**
+ * Main entry point for the Rize Digital website
+ * Renders the App component wrapped in HashRouter for GitHub Pages compatibility
+ */
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <HashRouter>
+//       <App />
+//     </HashRouter>
+//   </React.StrictMode>
+// );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
